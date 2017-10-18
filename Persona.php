853cc56cb94eba9a3875356d9cc30da55874d1d8
AhKhuzai <aalkhuzai.uqu@gmail.com>
2017-10-18 13:18:35 +0300

    implement part of persona

--- Persona.php
+++ Persona.php
@@ -15,7 +15,144 @@
 require_once 'User.php';
 
 class Persona extends User {
-
+    
+    private $ArName;
+    private $EnName;
+    private $uquId;
+    private $isUQU;
+    private $isTrainer;
+    private $special;
+    private $major;
+    private $resumeDir;
+    private $signDir;
+    private $gualification; 
+    private $phone; 
+    
+    function __construct() { 
+        require_once 'DB_Connector.php';
+        $this->connect = new DB_Connector();
+    }
+    
+    function __deconstruct() {    
+        $this->connect->closeConnection();
+    }
+    
+    function setArName($arName)
+    {
+        $this->ArName=$arName;
+    }
+    
+    function setEnName($EnName)
+    {
+        $this->EnName=$EnName;
+    }
+    
+    function setIsUQU($isUQU)
+    {
+        $this->isUQU=$isUQU;
+    }
+    
+    function setUQU($UQU)
+    {
+        $this->uquId=$UQU;
+    }
+    
+    function setIsTrainer($isTrainer)
+    {
+        $this->isTrainer=$isTrainer;
+    }
+    
+    function setPhone($Phone)
+    {
+        $this->phone=$Phone;
+    }
+    
+    function setResume($Resume)
+    {
+        $this->resumeDir=$Resume;
+    }
+    
+    function setSig($signeture)
+    {
+        $this->signDir=$signeture;
+    }
+    
+    function setSpecial($special)
+    {
+        $this->special=$special;
+    }
+    
+    function setMajor($major)
+    {
+        $this->major=$major;
+    }
+    
+    function setQualification($gualification)
+    {
+        $this->gualification=$gualification;
+    }
+    
+    // get func
+    
+        function getArName()
+    {
+        return $this->ArName;
+    }
+    
+    function getEnName()
+    {
+        return $this->EnName;
+    }
+    
+    function getIsUQU()
+    {
+        return $this->isUQU;
+    }
+    
+    function getUQU()
+    {
+        return $this->uquId;
+    }
+    
+    function getIsTrainer()
+    {
+        return $this->isTrainer;
+    }
+    
+    function getPhone()
+    {
+        return $this->phone;
+    }
+    
+    function getResume()
+    {
+        return $this->resumeDir;
+    }
+    
+    function getSig()
+    {
+        return $this->signDir;
+    }
+    
+    function getSpecial()
+    {
+        return $this->special;
+    }
+    
+    function getMajor()
+    {
+        return $this->major;
+    }
+    
+    function getQualification()
+    {
+        return $this->gualification;
+    }
+    
+    // DB Related fun
+    
+    //Server related function 
+     
     
 }
 
