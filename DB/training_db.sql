05f831ba2080cd7a58711c56b0e48c10ca136ea6
Ahkhuzai <32306401+Ahkhuzai@users.noreply.github.com>
2017-10-22 14:07:23 +0300

    NEW DB

--- DB/training_db.sql
+++ DB/training_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2017 at 10:14 AM
+-- Generation Time: Oct 22, 2017 at 12:57 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -19,8 +19,25 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tete`
+-- Database: `training_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+CREATE TABLE IF NOT EXISTS `attendance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usr_id` int(11) NOT NULL,
+  `timetable_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usr_id` (`usr_id`),
+  KEY `timetable_id` (`timetable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -28,6 +45,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `persona`
 --
 
+DROP TABLE IF EXISTS `persona`;
 CREATE TABLE IF NOT EXISTS `persona` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -35,13 +53,14 @@ CREATE TABLE IF NOT EXISTS `persona` (
   `ar_name` varchar(80) COLLATE utf8_bin NOT NULL,
   `eng_name` varchar(50) COLLATE utf8_bin NOT NULL,
   `contact_phone` varchar(10) COLLATE utf8_bin NOT NULL,
-  `contact_email` varchar(50) COLLATE utf8_bin NOT NULL,
-  `resume` blob,
+  `department` varchar(80) COLLATE utf8_bin NOT NULL,
+  `resume` text COLLATE utf8_bin,
   `qualification` text COLLATE utf8_bin,
   `major` varchar(20) COLLATE utf8_bin NOT NULL,
   `special` varchar(50) COLLATE utf8_bin NOT NULL,
   `is_trainer` tinyint(1) NOT NULL,
   `is_uqu_fac` tinyint(1) NOT NULL,
+  `signature` text COLLATE utf8_bin,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uqu_id` (`uqu_id`),
   KEY `user_id` (`user_id`)
@@ -50,10 +69,29 @@ CREATE TABLE IF NOT EXISTS `persona` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rate_details`
+-- Table structure for table `rate`
 --
 
-CREATE TABLE IF NOT EXISTS `rate_details` (
+DROP TABLE IF EXISTS `rate`;
+CREATE TABLE IF NOT EXISTS `rate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tc_id` int(11) NOT NULL,
+  `tr_id` int(11) NOT NULL,
+  `tr_total_avg_rate` double NOT NULL,
+  `tc_total_avg_rate` double NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tc_id_rate` (`tc_id`),
+  KEY `tr_id_rate` (`tr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratedetails`
+--
+
+DROP TABLE IF EXISTS `ratedetails`;
+CREATE TABLE IF NOT EXISTS `ratedetails` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rate_id` int(11) NOT NULL,
   `trainee_id` int(11) NOT NULL,
@@ -71,27 +109,11 @@ CREATE TABLE IF NOT EXISTS `rate_details` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rate_total`
+-- Table structure for table `ratevalue`
 --
 
-CREATE TABLE IF NOT EXISTS `rate_total` (
-  `rate_id` int(11) NOT NULL AUTO_INCREMENT,
-  `tc_id` int(11) NOT NULL,
-  `tr_id` int(11) NOT NULL,
-  `tr_total_avg_rate` double NOT NULL,
-  `tc_total_avg_rate` double NOT NULL,
-  PRIMARY KEY (`rate_id`),
-  KEY `tc_id_rate` (`tc_id`),
-  KEY `tr_id_rate` (`tr_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rate_value`
---
-
-CREATE TABLE IF NOT EXISTS `rate_value` (
+DROP TABLE IF EXISTS `ratevalue`;
+CREATE TABLE IF NOT EXISTS `ratevalue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ans` varchar(15) COLLATE utf8_bin NOT NULL,
   `value` int(11) NOT NULL,
@@ -99,10 +121,10 @@ CREATE TABLE IF NOT EXISTS `rate_value` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Dumping data for table `rate_value`
+-- Dumping data for table `ratevalue`
 --
 
-INSERT INTO `rate_value` (`id`, `ans`, `value`) VALUES
+INSERT INTO `ratevalue` (`id`, `ans`, `value`) VALUES
 (1, 'agree', 10),
 (2, 'diagree', 0),
 (3, 'somewhat ok', 5);
@@ -110,37 +132,21 @@ INSERT INTO `rate_value` (`id`, `ans`, `value`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `register_status`
+-- Table structure for table `registerstatus`
 --
 
-CREATE TABLE IF NOT EXISTS `register_status` (
-  `re_id` int(11) NOT NULL AUTO_INCREMENT,
-  `usr_id` int(11) NOT NULL,
-  `tc_id` int(11) NOT NULL,
-  `registration_status` int(11) NOT NULL,
-  PRIMARY KEY (`re_id`),
-  KEY `reg_co` (`usr_id`,`tc_id`),
-  KEY `registration_status` (`registration_status`),
-  KEY `tc_id` (`tc_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `status`
---
-
-CREATE TABLE IF NOT EXISTS `status` (
+DROP TABLE IF EXISTS `registerstatus`;
+CREATE TABLE IF NOT EXISTS `registerstatus` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(11) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `status`
+-- Dumping data for table `registerstatus`
 --
 
-INSERT INTO `status` (`id`, `status`) VALUES
+INSERT INTO `registerstatus` (`id`, `status`) VALUES
 (1, 'Processing'),
 (2, 'Accepted'),
 (3, 'Rejected'),
@@ -150,25 +156,30 @@ INSERT INTO `status` (`id`, `status`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tc_attendance`
+-- Table structure for table `registration`
 --
 
-CREATE TABLE IF NOT EXISTS `tc_attendance` (
+DROP TABLE IF EXISTS `registration`;
+CREATE TABLE IF NOT EXISTS `registration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `usr_id` int(11) NOT NULL,
-  `timetable_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  KEY `usr_id` (`usr_id`),
-  KEY `timetable_id` (`timetable_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `tc_id` int(11) NOT NULL,
+  `registration_status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `registration_status` (`registration_status`),
+  KEY `tc_id` (`tc_id`),
+  KEY `reg_co` (`usr_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tc_timetable`
+-- Table structure for table `timetable`
 --
 
-CREATE TABLE IF NOT EXISTS `tc_timetable` (
-  `tt_id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `timetable`;
+CREATE TABLE IF NOT EXISTS `timetable` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tc_id` int(11) NOT NULL,
   `tr_id` int(11) NOT NULL,
   `start_date` date NOT NULL,
@@ -176,7 +187,7 @@ CREATE TABLE IF NOT EXISTS `tc_timetable` (
   `duration` int(11) NOT NULL,
   `start_at` time NOT NULL,
   `location` varchar(50) NOT NULL,
-  PRIMARY KEY (`tt_id`),
+  PRIMARY KEY (`id`),
   KEY `trr_id` (`tr_id`),
   KEY `tc_id` (`tc_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -184,11 +195,12 @@ CREATE TABLE IF NOT EXISTS `tc_timetable` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `training_course`
+-- Table structure for table `trainingcourse`
 --
 
-CREATE TABLE IF NOT EXISTS `training_course` (
-  `tc_id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `trainingcourse`;
+CREATE TABLE IF NOT EXISTS `trainingcourse` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(80) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `goals` text CHARACTER SET utf8 COLLATE utf8_bin,
   `abstract` text CHARACTER SET utf8 COLLATE utf8_bin,
@@ -196,9 +208,10 @@ CREATE TABLE IF NOT EXISTS `training_course` (
   `status` tinyint(1) NOT NULL,
   `available_seat` int(11) DEFAULT NULL,
   `tr_id` int(11) NOT NULL,
-  PRIMARY KEY (`tc_id`),
+  `handoutDir` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `trainer_id` (`tr_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -206,19 +219,38 @@ CREATE TABLE IF NOT EXISTS `training_course` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(80) NOT NULL,
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `usrname` (`username`),
   UNIQUE KEY `usremail` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
+(23, '$use44rname', '$password', '$email8'),
+(26, '$username', '$password', '$email'),
+(30, '$username5', '$password', '$email10'),
+(31, '$username8', '$password', '$email6'),
+(35, '$username150', '$password', '$email128');
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`timetable_id`) REFERENCES `timetable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `persona`
@@ -227,46 +259,39 @@ ALTER TABLE `persona`
   ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `rate_details`
+-- Constraints for table `rate`
 --
-ALTER TABLE `rate_details`
-  ADD CONSTRAINT `rate_details_ibfk_1` FOREIGN KEY (`rate_id`) REFERENCES `rate_total` (`rate_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `rate_details_ibfk_2` FOREIGN KEY (`trainee_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `rate`
+  ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`tc_id`) REFERENCES `trainingcourse` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `rate_total`
+-- Constraints for table `ratedetails`
 --
-ALTER TABLE `rate_total`
-  ADD CONSTRAINT `rate_total_ibfk_1` FOREIGN KEY (`tc_id`) REFERENCES `training_course` (`tc_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `rate_total_ibfk_2` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `ratedetails`
+  ADD CONSTRAINT `ratedetails_ibfk_1` FOREIGN KEY (`rate_id`) REFERENCES `rate` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ratedetails_ibfk_2` FOREIGN KEY (`trainee_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `register_status`
+-- Constraints for table `registration`
 --
-ALTER TABLE `register_status`
-  ADD CONSTRAINT `register_status_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `register_status_ibfk_3` FOREIGN KEY (`registration_status`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `register_status_ibfk_4` FOREIGN KEY (`tc_id`) REFERENCES `training_course` (`tc_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `registration`
+  ADD CONSTRAINT `registration_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `registration_ibfk_3` FOREIGN KEY (`registration_status`) REFERENCES `registerstatus` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `registration_ibfk_4` FOREIGN KEY (`tc_id`) REFERENCES `trainingcourse` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `tc_attendance`
+-- Constraints for table `timetable`
 --
-ALTER TABLE `tc_attendance`
-  ADD CONSTRAINT `tc_attendance_ibfk_1` FOREIGN KEY (`usr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tc_attendance_ibfk_2` FOREIGN KEY (`timetable_id`) REFERENCES `tc_timetable` (`tt_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `timetable`
+  ADD CONSTRAINT `timetable_ibfk_1` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `timetable_ibfk_2` FOREIGN KEY (`tc_id`) REFERENCES `trainingcourse` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `tc_timetable`
+-- Constraints for table `trainingcourse`
 --
-ALTER TABLE `tc_timetable`
-  ADD CONSTRAINT `tc_timetable_ibfk_1` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tc_timetable_ibfk_2` FOREIGN KEY (`tc_id`) REFERENCES `training_course` (`tc_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `training_course`
---
-ALTER TABLE `training_course`
-  ADD CONSTRAINT `training_course_ibfk_1` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `trainingcourse`
+  ADD CONSTRAINT `trainingcourse_ibfk_1` FOREIGN KEY (`tr_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
